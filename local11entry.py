0860a52315934f7de695bb97a0c381e4b46f3a2b
Worm <mwormley008@gmail.com>
2023-07-05 12:44:24 -0500

    full

--- local11entry.py
+++ local11entry.py
@@ -2,8 +2,12 @@
 # This is a program that grabs information from a specified 
 # Excel column and then automates the input of that list into a data entry form
 # using autogui 
-import pyautogui, openpyxl
+import pyautogui, openpyxl, time
 from openpyxl import load_workbook
+from pyautogui import write, press
+from time import sleep
+
+sleep(3)
 
 workbook_path = r"\\WBR\shared\PAYROLL\WBR Payroll WE 2022\Local 11 June 2023.xlsx"
 
@@ -17,3 +21,8 @@ for cell in column_range:
     column_list.append(cell[0].value or "")
 
 print(column_list)
+
+for i in column_list:
+    print(i)
+    write(str(i))
+    press('tab')
